c28f3c5f9825699e17a296f09e37452d1ca5fb7c
BigCheese64 <50779153+BigCheese64@users.noreply.github.com>
2021-03-05 08:17:55 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,9 @@
-
+def deltaT(self):
+  for i in range(len(self.twitterStamps)):
+    if i!=len(self.twitterStamps):
+      self.deltaTy.append(self.twitterStamps[i+1]-self.twitterStamps[i])
+      self.deltaTx.append((self.twitterStamps[i+1]+self.twitterStamps[i])/2)
+      
+      
+    
+  
