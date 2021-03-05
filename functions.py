e174989b145e207d07ffffdd28f5ac550a6fea4e
BigCheese64 <50779153+BigCheese64@users.noreply.github.com>
2021-03-05 08:33:16 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,5 +5,11 @@ def deltaT(self):
       self.deltaTx.append((self.twitterStamps[i+1]+self.twitterStamps[i])/2)
       
       
-    
+def ytYvalues(self):
+  for i in self.youtubeStamps:
+    self.youtubeY.append(0)
+
+def coorilation(self):
+  for i in range(len(self.deltaTx)):
+    #delta T * avg tweettime = %closerToNext vid
   
